4f3e63f93730dc47331aee5ea52ad6accbf51b0e
lionelA-cloud <>
2025-11-07 20:28:20 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,12 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+
+# this excersice is to calculate the total cost of products through a lis of prices
+
+total = 0
+
+for price in prices:
+    print(price)
+    total += price
+
+print("Total price for products:", total)
+
