38b9722411ea0f16740e0c21d5b59a1e3a923825
lionelA-cloud <>
2025-11-07 20:17:49 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,9 @@
+item_name = "Bread"
+Item_Price = 4.52
+items_in_stock = 230
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", Item_Price)
+print("Quantity in stock:", items_in_stock)
