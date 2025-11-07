6eb2102493da6c10bb9f37a29b24dadbbf21c72f
lionelA-cloud <>
2025-11-07 20:20:43 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,8 @@
+# total cost of grocery bill
+total_cost = 25.00
+Discount_theshold = 20.00
+discountEligible = total_cost >= Discount_theshold
+
+eligible_for_discount = total_cost >= Discount_theshold
+
+print("Is the purchase eligible for a discount?", eligible_for_discount)
