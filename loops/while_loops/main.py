9beb52f53e7005d732e63bb875fddc32a7fc17bf
lionelA-cloud <>
2025-11-07 20:28:37 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,12 @@
+start_number = 5
+countdown_values = []
+
+# this is to demonstrate countdown timer that collects all countdown values in a list using a while loop
+
+while start_number >= 1:
+     countdown_values.append(start_number)
+     start_number -= 1
+
+print(F"Discount countdown complete!")
+
+print(countdown_values)
